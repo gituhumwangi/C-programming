c41a1dc3cb4180fdd5160eacd874bbabca6ddf06
dennis <dennis.mwangi@student.moringaschool.com>
2025-09-25 15:02:23 +0300

    Fixed error on temperature_converter

--- getting_started.c
+++ getting_started.c
@@ -1,18 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
+/*Convert celsius to fahrenheit. From 0 degrees celsius to 300 degrees celsius*/
 
-int main (int argc, [[maybe_unused]] char *argv[argc+1]) {
-    double A[5] = {
-        [0] = 9.0,
-        [1] = 2.9,
-        [4] = 3.E+25,
-        [3] = .00007,
-    };
-    for (size_t i = 0; i < 5; ++i) {
-        printf("element %zu is %g, \t its square is %g\n",
-        i,
-        A[i],
-        A[i] * A[i]);
-    };
-    return EXIT_SUCCESS;
+#include <stdio.h>
+
+int main(){
+    float celsius, fahrenheit;
+    float lower, upper, step;
+
+    lower = 0 ;
+    upper = 300;
+    step = 20;
+    celsius = lower;
+     
+    printf("%10s %10s\n", "Celsius", "Fahrenheit");
+    printf("_______________________\n");
+
+    while(celsius <= upper){
+        fahrenheit = (9.0/5.0) * (celsius - 32.0);
+        printf("%10.1f %10.1f\n", celsius, fahrenheit);
+        celsius = celsius +step;
+    }
+
 }

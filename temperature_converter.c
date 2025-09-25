c97b50f27a963aa6a56b2c1c74743fb498e614a3
dennis <dennis.mwangi@student.moringaschool.com>
2025-09-25 14:56:14 +0300

    celsius to fahrenhit converter

--- temperature_converter.c
+++ temperature_converter.c
@@ -20,3 +20,27 @@ int main(){
         fahrenheit = fahrenheit + step;
     }
 }
+
+/*Convert celsius to fahrenheit. From 0 degrees celsius to 300 degrees celsius*/
+
+#include <stdio.h>
+
+int main(){
+    float celsius, fahrenheit;
+    float lower, upper, step;
+
+    lower = 0 ;
+    upper = 300;
+    step = 20;
+    celsius = lower;
+     
+    printf("10%s 10%s\n", "Celsius", "Fahrenheit");
+    printf("_______________________\n");
+
+    while(celsius <= upper){
+        fahrenheit = (9.0/5.0) * (celsius - 32.0);
+        printf("%10.1f %10.1f\n", celsius, fahrenheit);
+        celsius = celsius +step;
+    }
+
+}
